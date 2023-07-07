53766fa6accd09129f517955e626cd8b162519a2
directormac <markasena@gmail.com>
2023-07-08 01:02:17 +0800

    fix subslash

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,8 +14,8 @@ const config = {
   url: "https://docs.mkra.dev",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
-  trailingSlash: true,
+  baseUrl: "/superdocs",
+  // trailingSlash: true,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "directormac", // Usually your GitHub org/user name.
